a90fb74a1535ab1ba0463794077ad68827ddc37e
mindfire <mindfire@mindfire-Vostro-3550>
2016-03-14 17:48:43 +0530

    merging acl and master branches

--- app/Http/Controllers/PrivilegeController.php
+++ app/Http/Controllers/PrivilegeController.php
@@ -8,55 +8,56 @@ use DB;
 
 class PrivilegeController extends Controller {
     function manageprivilege(Request $request) {
-			$role = $request['userid'];
-			$outputTable = '';
-	        $permissions = DB::table('permissions')
-	                         ->get();
-	        
-	        foreach($permissions as $row) {
-	            $actionInfo[] = $row;
-	        }
-	      
-		    //fetching the data from role_perm table 
-		    $rows = DB::table('role_perm')
-		               ->where('role_id', $role)
-		               ->get();
-		   
-		    foreach ($rows as $row) {
-		        $arr[$row->role_id . '-' . $row->resource_id . '-' . $row->permission_id] = true;
-		    }
-		    
-		    //Fetching the data from roles table and collect it into user
-		    $users = DB::table('roles')
-		               ->where('id', $role)
-		               ->get();
-		    foreach($users as $row) {
-		    	$user = $row->role_name;
-		    }
+		$role = $request['userid'];
+		$outputTable = '';
+        $permissions = DB::table('permissions')
+                         ->get();
+        
+        foreach($permissions as $row) {
+            $actionInfo[] = $row;
+        }
+      
+	    //fetching the data from role_perm table 
+	    $rows = DB::table('role_perm')
+	               ->where('role_id', $role)
+	               ->get();
+	   
+	    foreach ($rows as $row) {
+	        $arr[$row->role_id . '-' . $row->resource_id . '-' . $row->permission_id] = true;
+	    }
+	    
+	    //Fetching the data from roles table and collect it into user
+	    $users = DB::table('roles')
+	               ->where('id', $role)
+	               ->get();
+	    foreach($users as $row) {
+	    	$user = $row->role_name;
+	    }
 
-		    //Fetching id and name from Resource Table
-		    $rows = DB::table('resources')
-		              ->get();
-		    $count = 0;
-		    
-		    foreach ($rows as $resource ) {
-		    	$resources[$count]['id'] = $resource->id;
-		    	$resources[$count]['resource_name'] = 
-		    	$resource->resource_name;
-		    	$count =  $count + 1;
-		    }
+	    //Fetching id and name from Resource Table
+	    $rows = DB::table('resources')
+	              ->get();
+	    $count = 0;
+	    
+	    foreach ($rows as $resource ) {
+	    	$resources[$count]['id'] = $resource->id;
+	    	$resources[$count]['resource_name'] = 
+	    	$resource->resource_name;
+	    	$count =  $count + 1;
+	    }
 
-		    foreach ($resources as $resource) {
-		        $outputTable .= '<table id="res">
-		        <tr><td>'. $resource['resource_name'].'</td><td><td>&nbsp;&nbsp;&nbsp;</td><td>';
-		   
+	    foreach ($resources as $resource) {
+	        $outputTable .= '<table id="res">
+	        <tr><td>'. $resource['resource_name'].'</td><td><td>&nbsp;&nbsp;&nbsp;</td><td>';
+	   
 		    foreach ($actionInfo as $action) {
 		        $outputTable .='<input type="checkbox" '.'onchange="changeprivilege(this.checked,'.$role.','.$resource["id"].','.$action->id.') " name="check['. $resource['id'] .'][]" id="check" value="'  . $action->id  .' " '. (isset($arr[$role. '-' .$resource["id"]. '-' .$action->id] ) ? 'checked="checked"' : '').' /> '.$action->permission_name . '&nbsp;&nbsp;';
 		    }
-		        $outputTable .= '</td></tr></table>';
-		    }
+		    
+	        $outputTable .= '</td></tr></table>';
+		}
 
-		    return  $outputTable;
+	    return  $outputTable;
 		}
 
 	    function privilegpost(Request $request) {
